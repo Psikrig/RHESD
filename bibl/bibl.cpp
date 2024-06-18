19cc02964e57a53536faf3cf04f859a60ac88402
Psikrig <psikrieg@gmail.com>
2024-06-18 10:55:06 +0300

    first commit

---
+++ bibl/bibl.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <string>
+using namespace std;
+
+extern "C" __declspec(dllexport) bool containsAllCharacters(const char* str, const char* chars) {
+    string s(str);
+    string c(chars);
+
+    for (char ch : c) {
+        if (s.find(ch) == string::npos) {
+            return false;
+        }
+    }
+    return true;
+}
